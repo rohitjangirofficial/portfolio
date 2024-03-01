cc3e73261b724c98162a0da7b7b3de77b758ffb7
Rohit Jangir <rohitjangirofficial@gmail.com>
2024-03-01 18:18:06 +0530

    modified

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,9 +12,21 @@ export const Hero = () => {
           I'm a full-stack developer with 1.5+ years of experience in React JS
           and Node JS. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.actionBtns}>
+          <a
+            href="mailto:rohitjangirofficial@email.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+          <a href="https://wa.me/918209083798" className={styles.whatsappBtn}>
+            <img
+              src={getImageUrl("hero/whatsapp.png")}
+              alt="whatsapp"
+              // className={styles.heroImg}
+            />
+          </a>
+        </div>
       </div>
       {/* <img
         src={getImageUrl("hero/heroImage.png")}
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import "@fontsource/roboto";
 import "@fontsource/space-mono";
 import "@fontsource/poppins";
 import "@fontsource/montserrat";
+import "@fontsource/sora";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
