98924f0c782afe2f3598d276a6cd9a26d320c6f3
Rohit Jangir <rohitjangirofficial@gmail.com>
2024-03-01 15:51:14 +0530

    changed config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-deploy/",
   css: {
     modules: {
       localsConvention: "camelCase",
-      base: "/vite-deploy/"
     },
   },
 });
